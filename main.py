1d89b02c0a1fc1f23e5c32f00d8f8fd74e754ab0
Gemaljevic Aleksandar <67740316+onlyfaker@users.noreply.github.com>
2025-05-19 23:59:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import pygame,sys
 
+# clock limits how fast our loop runs
+# Going over the pygame documentation
 
 pygame.init()
-# Going over the pygame documentation
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((400, 400))
+clock = pygame.time.Clock()
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running=False
-            sys.exit()
+# pygame.display.flip()	- for larger screen update
     pygame.display.update()
+    clock.tick(60)
+
+pygame.quit()
+sys.exit()
