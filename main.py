8945ac73190e70779173059405a7ad410a51c5e4
Gemaljevic Aleksandar <67740316+onlyfaker@users.noreply.github.com>
2025-05-20 21:32:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,20 +9,15 @@ clock = pygame.time.Clock()
 
 
 test_surface = pygame.Surface((100,100))#size
-
-
 # test_rect = pygame.Rect(150,200,100,100) #creating a new rect
 test_old_rect = test_surface.get_rect(center = (200,200))
-# y_pos = 200
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running=False
 
-    # y_pos+=1
     screen.fill((100,23,87))
-
     screen.blit(test_surface,test_old_rect)#position
 
     # pygame.draw.ellipse(surface=screen,color = "white",rect=test_rect)# drawing with new rect
