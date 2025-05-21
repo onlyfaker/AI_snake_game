e642390184bfd8db65e4457e3e59c52c121dafc2
Gemaljevic Aleksandar <67740316+onlyfaker@users.noreply.github.com>
2025-05-20 21:31:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,17 +8,25 @@ screen = pygame.display.set_mode((400, 400))
 clock = pygame.time.Clock()
 
 
-test_surface = pygame.Surface((400,200))#size
-y_pos = 200
+test_surface = pygame.Surface((100,100))#size
+
+
+# test_rect = pygame.Rect(150,200,100,100) #creating a new rect
+test_old_rect = test_surface.get_rect(center = (200,200))
+# y_pos = 200
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running=False
 
-    y_pos+=1
+    # y_pos+=1
     screen.fill((100,23,87))
-    screen.blit(test_surface,(0,y_pos))#position
+
+    screen.blit(test_surface,test_old_rect)#position
+
+    # pygame.draw.ellipse(surface=screen,color = "white",rect=test_rect)# drawing with new rect
+
 # pygame.display.flip()	- for larger screen update
     pygame.display.update()
     clock.tick(60)#max 60 fps
