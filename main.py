a252ecd302bd8846da4dd217df8d157cfd42779b
Gemaljevic Aleksandar <67740316+onlyfaker@users.noreply.github.com>
2025-05-20 21:04:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,16 @@ pygame.init()
 screen = pygame.display.set_mode((400, 400))
 clock = pygame.time.Clock()
 
+
+test_surface = pygame.Surface((100,200))
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running=False
+
+    screen.fill((100,23,87))
+    screen.blit(test_surface,(200,150))
 # pygame.display.flip()	- for larger screen update
     pygame.display.update()
     clock.tick(60)#max 60 fps
