708f0af099ce560b01ed09998c7fd69677ee6de6
Gemaljevic Aleksandar <67740316+onlyfaker@users.noreply.github.com>
2025-05-23 22:14:18 -0400

    snake creation update
    next is movement of snake

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-import pygame,sys
+import pygame,sys,random
 from pygame.math import Vector2
 
 # TODO - transform the turtle snake logic to pygame snake logic
+
+class Snake():
+    def __init__(self):
+        self.body = [Vector2(5,10),Vector2(6,10),Vector2(7,10)]
+
+    def draw_snake(self):
+        for block in self.body:
+            x_pos = block.x*cell_size
+            y_pos = block.y*cell_size
+            body_rect = pygame.Rect(x_pos,y_pos,cell_size,cell_size)
+            pygame.draw.rect(screen,(155,15,155),body_rect)
 class Food():
     def __init__(self):
-        self.x = 5
-        self.y = 4
+        self.x = random.randint(0,cell_number-1)
+        self.y = random.randint(0,cell_number-1)
         self.pos = Vector2(self.x,self.y)
     def draw_fruit(self):
-        fruit_rect = pygame.Rect(self.pos.x*cell_size,self.pos.y *cell_size,cell_size,cell_size)
+        fruit_rect = pygame.Rect(self.pos.x*cell_size,self.pos.y *cell_size,cell_size,cell_size)#add int() if there is an errror here
         pygame.draw.rect(screen,(44,44,44),fruit_rect)
 # clock limits how fast our loop runs
 # Going over the pygame documentation
@@ -20,6 +31,7 @@ screen = pygame.display.set_mode((cell_size*cell_number, cell_size*cell_number))
 clock = pygame.time.Clock()
 
 fruit = Food()
+snake = Snake()
 
 running = True
 while running:
@@ -28,7 +40,9 @@ while running:
             running=False
 
     screen.fill((111, 148, 118))
+
     fruit.draw_fruit()
+    snake.draw_snake()
     pygame.display.update()
 # max 60 fps
     clock.tick(60)
